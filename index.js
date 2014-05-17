b04f3923809d8b131248e9a5a1359c3950beaacc
Brandon Tilley <brandon@brandontilley.com>
2014-05-16 23:42:17 -0700

    Add Flux.StoreWatchMixin, rename Flux.Mixin to Flux.FluxMixin

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 var Dispatcher = require("./lib/dispatcher"),
     Flux = require("./lib/flux"),
     FluxMixin = require("./lib/flux_mixin"),
+    StoreWatchMixin = require("./lib/store_watch_mixin"),
     createStore = require("./lib/create_store");
 
 var Fluxbox = {
   Dispatcher: Dispatcher,
   Flux: Flux,
-  Mixin: FluxMixin,
+  FluxMixin: FluxMixin,
+  StoreWatchMixin: StoreWatchMixin,
   createStore: createStore
 };
 
