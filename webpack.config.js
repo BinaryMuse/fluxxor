9732314855f2766a7f568f0ebdab07512d24f50a
Brandon Tilley <brandon@brandontilley.com>
2014-05-18 01:05:03 -0700

    Add browser build script

---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+var webpack = require("webpack");
+
+module.exports = {
+  cache: true,
+  entry: "./index.js",
+  sourceMapFilename: "fluxbox.js.min.map",
+  minimize: true,
+  output: {
+    path: __dirname + "/build",
+    filename: "fluxbox.min.js",
+    library: "Fluxbox",
+    libraryTarget: "umd"
+  },
+  devtool: "source-map",
+  module: {
+    loaders: [
+      { test: /\.less$/, loader: "style!css!less" },
+      { test: /\.jsx$/, loader: "jsx-loader" }
+    ]
+  }
+};
